d9a89ad3fd257522a96d00ec6563103408b56f04
PinyinCode <160508972+PinyinCode@users.noreply.github.com>
2026-08-26 12:55:04 +0700

    Update max98357a_codec.h

--- main/audio/codecs/max98357a_codec.h
+++ main/audio/codecs/max98357a_codec.h
@@ -3,16 +3,11 @@
 
 #include "audio_codec.h"
 #include <driver/i2s_std.h>
-#include <esp_log.h>
 #include <vector>
 #include <mutex>
-#include <algorithm>
-
-#define TAG "Max98357aCodec"
 
 class Max98357aCodec : public AudioCodec {
 private:
-    // GPIO pins
     gpio_num_t bclk_pin_;
     gpio_num_t lrck_pin_;
     gpio_num_t dout_pin_;
@@ -20,15 +15,12 @@ private:
     gpio_num_t ws_pin_;
     gpio_num_t din_pin_;
     
-    // I2S handles
     i2s_chan_handle_t tx_handle_ = nullptr;
     i2s_chan_handle_t rx_handle_ = nullptr;
     
-    // State
     bool started_ = false;
-    bool mute_ = false;
     std::mutex data_if_mutex_;
-    std::vector<int16_t> scaled_buffer_;  // Buffer for volume scaling
+    std::vector<int16_t> scaled_buffer_;
 
     void CreateDuplexChannels();
 
@@ -36,17 +28,15 @@ public:
     Max98357aCodec(int input_sample_rate, int output_sample_rate,
                    gpio_num_t bclk, gpio_num_t lrck, gpio_num_t dout,
                    gpio_num_t sck, gpio_num_t ws, gpio_num_t din);
-    
-    ~Max98357aCodec();
+    virtual ~Max98357aCodec();
 
-    // AudioCodec interface
-    bool Start() override;
-    void Stop() override;
-    int Read(int16_t* dest, int samples) override;
-    int Write(const int16_t* data, int samples) override;
-    void SetOutputVolume(int volume) override;
-    void EnableInput(bool enable) override;
-    void EnableOutput(bool enable) override;
+    virtual void SetOutputVolume(int volume) override;
+    virtual void EnableInput(bool enable) override;
+    virtual void EnableOutput(bool enable) override;
+
+private:
+    virtual int Read(int16_t* dest, int samples) override;
+    virtual int Write(const int16_t* data, int samples) override;
 };
 
 #endif // MAX98357A_CODEC_H
