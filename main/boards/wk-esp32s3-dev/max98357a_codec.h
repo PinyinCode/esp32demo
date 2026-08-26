4bccd3fd227773d9333b4ff784a886a72b1d44f6
PinyinCode <160508972+PinyinCode@users.noreply.github.com>
2026-08-26 13:36:37 +0700

    Create max98357a_codec.h

---
+++ main/boards/wk-esp32s3-dev/max98357a_codec.h
@@ -0,0 +1,36 @@
+#ifndef MAX98357A_CODEC_H
+#define MAX98357A_CODEC_H
+
+#include "audio_codec.h"
+#include <driver/i2s_std.h>
+
+class Max98357aCodec : public AudioCodec {
+private:
+    gpio_num_t bclk_pin_;
+    gpio_num_t lrck_pin_;
+    gpio_num_t dout_pin_;
+    gpio_num_t sck_pin_;
+    gpio_num_t ws_pin_;
+    gpio_num_t din_pin_;
+    
+    i2s_chan_handle_t tx_handle_ = nullptr;
+    i2s_chan_handle_t rx_handle_ = nullptr;
+
+    void CreateDuplexChannels();
+
+public:
+    Max98357aCodec(int input_sample_rate, int output_sample_rate,
+                   gpio_num_t bclk, gpio_num_t lrck, gpio_num_t dout,
+                   gpio_num_t sck, gpio_num_t ws, gpio_num_t din);
+    virtual ~Max98357aCodec();
+
+    virtual void SetOutputVolume(int volume) override;
+    virtual void EnableInput(bool enable) override;
+    virtual void EnableOutput(bool enable) override;
+
+protected:
+    virtual int Read(int16_t* dest, int samples) override;
+    virtual int Write(const int16_t* data, int samples) override;
+};
+
+#endif
