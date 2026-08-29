dc4ec4ffe004efa19f044c06640361d2a805e9e6
PinyinCode <160508972+PinyinCode@users.noreply.github.com>
2026-08-29 11:27:31 +0700

    Delete main/boards/wk-esp32s3-dev/license_config.h

--- main/boards/wk-esp32s3-dev/license_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-
-inline int64_t GetEncryptedExpirationTimestamp() {
-    uint64_t obfuscated_val = 1798761600ULL ^ 0x5A5A5A5AULL; 
-    uint64_t secret_key = 0x5A5A5A5AULL;
-    return (int64_t)(obfuscated_val ^ secret_key);
-}
