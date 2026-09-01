d22fba3513e0248e3953aa88b2546431b0644400
PinyinCode <160508972+PinyinCode@users.noreply.github.com>
2026-09-01 16:13:50 +0700

    Update project_config.h

--- main/boards/wk-esp32s3-dev/project_config.h
+++ main/boards/wk-esp32s3-dev/project_config.h
@@ -4,6 +4,7 @@
 // Cấu hình máy chủ chính
 #define SERVER_BASE_URL          "https://esp32-bank-speaker.onrender.com"
 #define OTA_SERVER_URL           "https://esp32-ota-server-9yuy.onrender.com"
+#define LOOKUP_SERVER_URL        "https://tracuu-a3n0.onrender.com"          // Link Render tra cứu mới của bạn
 
 // Các Endpoint API
 #define API_CHECK_BANK_AUDIO     "/api/check-bank-audio"
@@ -12,5 +13,6 @@
 #define API_TRIGGER_EMAIL        "/api/trigger-check-email"
 #define API_CHECK_LICENSE        "/api/check-license"
 #define API_CHECK_UPDATE         "/api/check-update"
+#define API_DATA_LOOKUP          "/api/info/lookup"                          // Endpoint tra cứu tổng hợp
 
 #endif // PROJECT_CONFIG_H
